1488751dc786d39102ad90858626465485cf05aa
JulesVNHS <98834604+JulesVNHS@users.noreply.github.com>
2024-03-17 19:17:51 +0100

    Warning Formulaire
    Empèche de valider le formulaire si pas de train + message d'erreur

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -38,118 +38,133 @@ fetch('assets/data_duree_retard.json')
     })
     .catch(error => console.error('Erreur lors du chargement du JSON :', error));
 
-    /*résultat*/
-    function loadJSON(callback) {
-        var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'assets/data_duree_retard.json', true);
-        xobj.onreadystatechange = function () {
-            if (xobj.readyState == 4) {
-                if (xobj.status == 200) {
-                    callback(null, xobj.responseText);
-                } else {
-                    callback(new Error('Error loading JSON: ' + xobj.statusText));
-                }
-            }
-        };
-        xobj.onerror = function() {
-            callback(new Error('Network error occurred while loading JSON'));
-        };
-        xobj.send();
-    }
-
-    function countMatchingElements(data, departure, arrival) {
-        var count = 0;
-        var totalDelay = 0;
-    
-        var depToLower = departure.toLowerCase();
-        var arrToLower = arrival.toLowerCase();
-    
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].zone_depart.toLowerCase() === depToLower && data[i].destination.toLowerCase() === arrToLower) {
-                count++;
-                totalDelay += data[i].retard_arr_sec;
+/*résultat*/
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'assets/data_duree_retard.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4) {
+            if (xobj.status == 200) {
+                callback(null, xobj.responseText);
+            } else {
+                callback(new Error('Error loading JSON: ' + xobj.statusText));
             }
         }
-    
-        var averageDelay = count > 0 ? Math.round(totalDelay / count) : 0;
-    
-        return { averageDelay: averageDelay };
+    };
+
+    xobj.onerror = function() {
+        callback(new Error('Network error occurred while loading JSON'));
+    };
+    xobj.send();
+}
+
+function countMatchingElements(data, departure, arrival) {
+    var count = 0;
+    var totalDelay = 0;
+
+    var depToLower = departure.toLowerCase();
+    var arrToLower = arrival.toLowerCase();
+
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].zone_depart.toLowerCase() === depToLower && data[i].destination.toLowerCase() === arrToLower) {
+            count++;
+            totalDelay += data[i].retard_arr_sec;
+        }
     }
 
-    document.querySelector('.form-retard').addEventListener('submit', function(event) {
-        event.preventDefault();
+    var averageDelay = count > 0 ? Math.round(totalDelay / count) : 0;
 
-        var departureInput = document.getElementById("form-retard__departure-input");
-        var arrivalInput = document.getElementById("form-retard__arrival-input");
-        var frequencyInput = document.getElementById("form-retard__frequency-input");
-        var departure = departureInput.value.trim();
-        var arrival = arrivalInput.value.trim();
-        var frequency = parseInt(frequencyInput.value.trim());
+    return { averageDelay: averageDelay };
+}
 
-        if (!departure || !arrival) {
-            console.error("Veuillez entrer à la fois une zone de départ et une destination.");
+document.querySelector('.form-retard').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var departureInput = document.getElementById("form-retard__departure-input");
+    var arrivalInput = document.getElementById("form-retard__arrival-input");
+    var frequencyInput = document.getElementById("form-retard__frequency-input");
+    var departure = departureInput.value.trim();
+    var arrival = arrivalInput.value.trim();
+    var frequency = parseInt(frequencyInput.value.trim());
+    if (!departure || !arrival) {
+        console.error("Veuillez entrer à la fois une zone de départ et une destination.");
+        return;
+    }
+
+    loadJSON(function(error, response) {
+        if (error) {
+            console.error(error);
             return;
         }
-
-        loadJSON(function(error, response) {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            try {
-                var jsonData = JSON.parse(response);
-                var result = countMatchingElements(jsonData, departure, arrival);
-                document.getElementById('averageDelay').textContent = convertSecondsToTime(result.averageDelay);
-
-                var monthlyEstimation = Math.round(result.averageDelay * frequency * 4);
-                var yearlyEstimation = Math.round(result.averageDelay * frequency * 52);
-                document.getElementById('monthlyEstimation').textContent = convertSecondsToTime(monthlyEstimation);
-                document.getElementById('yearlyEstimation').textContent = convertSecondsToTime(yearlyEstimation);
-            } catch (parseError) {
-                console.error("Erreur lors de l'analyse de la réponse JSON :", parseError);
-            }
-        });
+        try {
+            var jsonData = JSON.parse(response);
+            var result = countMatchingElements(jsonData, departure, arrival);
+            document.getElementById('averageDelay').textContent = convertSecondsToTime(result.averageDelay);
+            var monthlyEstimation = Math.round(result.averageDelay * frequency * 4);
+            var yearlyEstimation = Math.round(result.averageDelay * frequency * 52);
+            document.getElementById('monthlyEstimation').textContent = convertSecondsToTime(monthlyEstimation);
+            document.getElementById('yearlyEstimation').textContent = convertSecondsToTime(yearlyEstimation);
+        } catch (parseError) {
+            console.error("Erreur lors de l'analyse de la réponse JSON :", parseError);
+        }
     });
+});
 
-    function convertSecondsToTime(seconds) {
-        var hours = Math.floor(seconds / 3600);
-        var minutes = Math.floor((seconds % 3600) / 60);
-        var remainingSeconds = seconds % 60;
-
-        var timeString = '';
-
-        if (hours > 0) {
-            timeString += hours + 'h ';
-        }
-
-        if (minutes > 0) {
-            timeString += minutes + 'min ';
-        }
-
-        if (remainingSeconds > 0) {
-            timeString += remainingSeconds + 's';
-        }
-
-        return timeString.trim();
+function convertSecondsToTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var remainingSeconds = seconds % 60;
+    var timeString = '';
+    if (hours > 0) {
+        timeString += hours + 'h ';
     }
+    if (minutes > 0) {
+        timeString += minutes + 'min ';
+    }
+    if (remainingSeconds > 0) {
+        timeString += remainingSeconds + 's';
+    }
+    return timeString.trim();
+}
 
+/*validation form*/
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('.form-retard');
+    var sectionResult = document.querySelector('.result');
 
-    /*validation form*/
-    document.addEventListener('DOMContentLoaded', function() {
-        var form = document.querySelector('.form-retard');
-        var sectionResult = document.querySelector('.result');
-    
-        if (form && sectionResult) {
-            form.addEventListener('submit', function(event) {
-                event.preventDefault();
-                
-                form.classList.add('form-retard--hide');
-                
-                sectionResult.classList.remove('result--hide');
-            });
-        } else {
-            console.error("Les éléments .form-retard ou .result n'ont pas été trouvés.");
-        }
+    if (form && sectionResult) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            form.classList.add('form-retard--hide');
+            
+            sectionResult.classList.remove('result--hide');
+        });
+    } else {
+        console.error("Les éléments .form-retard ou .result n'ont pas été trouvés.");
+    }
+});
+
+/*vérification itinéraire*/
+document.querySelectorAll('.form-retard__input').forEach(function(input) {
+    input.addEventListener('change', function() {
+        var departure = document.getElementById('form-retard__departure-input').value.toUpperCase();
+        var arrival = document.getElementById('form-retard__arrival-input').value.toUpperCase();
+
+        fetch('assets/data_duree_retard.json')
+            .then(response => response.json())
+            .then(data => {
+                var trainExists = data.some(train => train.zone_depart.toUpperCase() === departure && train.destination.toUpperCase() === arrival);
+                var submitButton = document.querySelector('.form-retard__submit');
+                submitButton.disabled = !trainExists;
+
+                var warningMessage = document.querySelector('.form-retard__warning-message');
+                if (!trainExists) {
+                    warningMessage.textContent = 'Aucun train avec cet itinéraire n\'existe.';
+                } else {
+                    warningMessage.textContent = '';
+                }
+            })
+            .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
     });
+});
